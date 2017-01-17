faec241306a85fe6224a73ee142e9adf66dc5ac6
Dan Nelson <dnelson@cybertooth.ca>
2017-01-16 22:09:01 -0700

    #1 A new `div.form-group` component that will present model field errors when they present themselves.

--- tests/dummy/app/models/demo-model.js
+++ tests/dummy/app/models/demo-model.js
@@ -1,7 +1,18 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  anotherString: DS.attr('string'),
   someBoolean: DS.attr('boolean'),
   someNumber: DS.attr('number'),
-  someString: DS.attr('string')
+  someString: DS.attr('string'),
+  _addErrorToAnotherString: Ember.observer('anotherString', function () {
+    if (this.get('anotherString') === 'error') {
+      this.get('errors')._add('anotherString', 'Now change the text to error2 to add a second');
+    } else if (this.get('anotherString') === 'error2') {
+      this.get('errors')._add('anotherString', 'Another error, take it easy');
+    } else {
+      this.get('errors')._remove('anotherString');
+    }
+  })
 });
